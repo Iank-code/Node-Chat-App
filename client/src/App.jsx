0d3aecfddce90d93451da9a45d935c0d50a11f6d
Ian Kamau <kian99564@gmail.com>
2023-09-07 16:27:02 +0300

    Project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <h3>Hello world</h3>;
+}
+
+export default App;
---
+++ client/src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Importing components
+import Join from "./components/Join.jsx";
+import Chat from "./components/Chat.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Join />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
