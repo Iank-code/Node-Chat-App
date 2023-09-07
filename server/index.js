22d4c9c212af8f18bf5bef5de0068eb043538b05
Ian Kamau <kian99564@gmail.com>
2023-09-07 16:35:28 +0300

    Minor changes

--- server/index.js
+++ server/index.js
@@ -10,7 +10,15 @@ const router = require("./router");
 
 const PORT = process.env.PORT || 5000;
 
-app.use(router)
+io.on("connection", (socket) => {
+  console.log("We have a new connectionq");
+
+  socket.on("disconnect", () => {
+    console.log("User just disconnected");
+  });
+});
+
+app.use(router);
 server.listen(PORT, () => {
   console.log(`Server has started on port ${PORT}`);
 });
