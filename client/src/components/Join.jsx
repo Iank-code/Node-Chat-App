6853508fc3c422d244d3c42cea6be2c6df69c285
Ian Kamau <kian99564@gmail.com>
2023-09-07 16:48:04 +0300

    Styling join.jsx

--- client/src/components/Join.jsx
+++ client/src/components/Join.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./join.css";
 
 export default function Join() {
-  return <p>Join</p>;
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+
+        <div>
+          <input
+            className="joinInput"
+            type="text"
+            placeholder=""
+            onChange={""}
+          />
+        </div>
+
+        <div>
+          <input
+            className="joinInput"
+            type="text"
+            placeholder=""
+            onChange={""}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
